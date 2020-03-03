3fff32e2bfde8906866623f13bc247e37cb5759e
Rodrigues <fr.costa95@gmail.com>
2020-03-03 07:48:21 -0300

    feat(street): remove street scaffold

--- app/models/street.rb
+++
@@ -1,3 +0,0 @@
-class Street < ApplicationRecord
-  belongs_to :customer
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   resources :monthlies
-  resources :streets
   resources :costumers
   resources :cashes
 
